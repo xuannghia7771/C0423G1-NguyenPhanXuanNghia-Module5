87e8825216a3f25ea5735c8eb05fccd1ef39b5c9
DELL <nghiaphan33dn@gmail.com>
2023-09-18 19:35:17 +0700

    oke

---
+++ ss2_ReactJS_And_JSX/exercise/student-information/student/src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  const students = [
+    {
+      company: 'Alfreds Futterkiste',
+      contact: 'Maria Anders',
+      country: 'Germany'
+    },
+    {
+      company: 'Centro comercial Moctezuma',
+      contact: 'Francisco Chang',
+      country: 'Mexico'
+    },
+    {
+      company: 'Ernst Handel',
+      contact: 'Roland Mendel',
+      country: 'Austria'
+    },
+    {
+      company: 'Island Trading',
+      contact: 'Helen Bennett',
+      country: 'UK'
+    },
+    {
+      company: 'Laughing Bacchus Winecellars',
+      contact: 'Yoshi Tannamuri',
+      country: 'Canada'
+    },
+    {
+      company: 'Magazzini Alimentari Riuniti',
+      contact: 'Giovanni Rovelli',
+      country: 'Italy'
+    }
+  ]
+  return (
+      <>
+        <h1>Students</h1>
+        <table>
+          <tr>
+            <th>company</th>
+            <th>contact</th>
+            <th>country</th>
+          </tr>
+          {students.map(student => (
+              <tr>
+                <td>{student.company}</td>
+                <td>{student.contact}</td>
+                <td>{student.country}</td>
+              </tr>
+          ))}
+        </table>
+      </>
+  );
+}
+
+export default App;
